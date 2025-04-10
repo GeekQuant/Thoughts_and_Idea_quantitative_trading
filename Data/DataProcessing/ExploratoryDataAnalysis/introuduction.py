81f11f5049b7726ab3ae5a7e445dad58e8ce49ef
Congran Lu <102529259+GeekQuant@users.noreply.github.com>
2025-04-10 17:53:41 +0800

    Update introuduction.py

--- Data/DataProcessing/ExploratoryDataAnalysis/introuduction.py
+++ Data/DataProcessing/ExploratoryDataAnalysis/introuduction.py
@@ -1,1 +1,3 @@
 This session includes example codes for data explortary analysis and data visualisation technique. 
+Some techniques are from different book and paper. 
+《Exploratory Data Analysis with Python Cookbook》
